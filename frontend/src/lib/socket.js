f8a2e97d4c3e6075883730c36a9464a07646b798
Soham Mondal <158185340+sohammondal29@users.noreply.github.com>
2026-05-13 00:18:29 +0530

    Update socket.js

--- frontend/src/lib/socket.js
+++ frontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://codenova-green.vercel.app/";
+const SOCKET_URL = "https://codenova-rmy8.onrender.com/";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: true,
