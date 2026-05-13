fa5c22e0e16e80c0f64de936b76838dbffcc0cd8
Soham Mondal <158185340+sohammondal29@users.noreply.github.com>
2026-05-13 14:30:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from "react-router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+import { setupAxiosInterceptors } from "./lib/axios";
+
+// Clerk publishable key
+const PUBLISHABLE_KEY =
+  import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk Publishable Key");
@@ -15,14 +23,25 @@ if (!PUBLISHABLE_KEY) {
 
 const queryClient = new QueryClient();
 
+// Setup Axios Auth Interceptor
+function AxiosInterceptorSetup({ children }) {
+  const { getToken } = useAuth();
+
+  setupAxiosInterceptors(getToken);
+
+  return children;
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-          <App />
-        </ClerkProvider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <AxiosInterceptorSetup>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </BrowserRouter>
+      </AxiosInterceptorSetup>
+    </ClerkProvider>
   </StrictMode>
 );
