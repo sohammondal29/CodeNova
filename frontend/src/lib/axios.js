4e36a5c7ff8363104214a5c921cff5b0f6fa284a
Soham Mondal <158185340+sohammondal29@users.noreply.github.com>
2026-05-13 14:28:52 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,20 @@
 import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
+
+export const setupAxiosInterceptors = async (getToken) => {
+  axiosInstance.interceptors.request.use(async (config) => {
+    const token = await getToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  });
+};
 
 export default axiosInstance;
